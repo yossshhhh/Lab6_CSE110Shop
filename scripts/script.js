526eb2d5989ffaafd4911d9a736d08cea7fef1be
yossshhhh <55852579+yossshhhh@users.noreply.github.com>
2021-02-13 12:57:08 -0800

    added comments + changed let to var

--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,26 @@
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
   
-  var local = window.localStorage;
-  let productList = document.getElementById('product-list');
-  let numItemsInCart = document.getElementById('cart-count');
+  let local = window.localStorage;
+    //var bc need global scope
+  var productList = document.getElementById('product-list');
+  var numItemsInCart = document.getElementById('cart-count');
   
+  //check if need to fetch data from website
   if (local.getItem('products') != null) {
+      //parse existing data bc non null
       let productData = JSON.parse(local.getItem('products'));
+      //loop through every product in list and create custom element for each
       for(let i = 0; i < productData.length; i ++) {
+        //use constructor from product-item.js
         var data = new ProductItem(productData[i]['id'], productData[i]['image'], productData[i]['description'], productData[i]['title'], productData[i]['price']);
         productList.appendChild(item);
       }
   }
   else {
+    //retrieve raw data from webstite
     fetch('https://fakestoreapi.com/products')
+      //same as above, except with fetched data variables
       .then(response => response.json())
       .then(data => {
         local.setItem('products', JSON.stringify(data));
@@ -27,9 +34,11 @@ window.addEventListener('DOMContentLoaded', () => {
       })
   }
   
+  //add to array
   if(!local.getItem('cartContent'))
     local.setItem('cartContent', JSON.stringify([]));
   
+  //update counter
   var cartContent = JSON.parse(local.getItem('cartContent'));
   numItemsInCart.textContent - cartContent.length;
 });
